63808349b7bf6358861b676636a7fbbfee5351a7
Dima Sidenko <sidenkodima2202@gmail.com>
2025-12-27 19:39:19 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const fieldElements = [...document.querySelectorAll('.field')];
+
+function firstLetterToUpperCase(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+fieldElements.map((field) => {
+  const label = document.createElement('label');
+  const input = field.lastElementChild;
+  const attributeName = input.getAttribute('name');
+  const attributeId = input.getAttribute('id');
+
+  input.setAttribute('placeholder', firstLetterToUpperCase(attributeName));
+  label.textContent = attributeName.toUpperCase();
+  label.setAttribute('for', attributeId);
+  label.classList.add('field-label');
+  field.prepend(label);
+});
